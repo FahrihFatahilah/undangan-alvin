f591e0380bbe9a0e7c7596cc3d05ef96674c5eaf
ffatahilah <ffatahilah@bankbjb.co.id>
2026-05-25 11:53:13 +0700

    nama ortu

--- resources/views/invitation/sections/footer.blade.php
+++ resources/views/invitation/sections/footer.blade.php
@@ -21,13 +21,13 @@
 
         <div class="mt-16 pt-8 border-t border-secondary/20">
             <p class="font-body text-sm text-text-dark/50">
-                © dammm | invitation · 
+                
                 <a 
                     href="https://instagram.com/ffatahilah__" 
                     target="_blank" 
                     class="hover:text-primary transition-colors"
                 >
-                    Instagram
+                    © dammm | invitation · 
                 </a>
             </p>
         </div>
